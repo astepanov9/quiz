973245b8822d6934850086fc20e1bb1d942341d3
astepanov9 <mileez@yandex.ru>
2022-02-03 20:43:27 +0300

    quiz

---
+++ data.js
@@ -0,0 +1,65 @@
+const quizData = [{
+    number: 1,
+    title: "На какую сумму вы рассчитываете?",
+    answer_alias: "money",
+    answers: [{
+            answer_title: "500 рублей",
+            type: "checkbox"
+        },
+        {
+            answer_title: "5000 рублей",
+            type: "checkbox"
+        },
+        {
+            answer_title: "Введу текстом",
+            type: "text"
+        }
+    ]
+},
+{
+    number: 2,
+    title: "Какой именно вам нужен сайт?",
+    answer_alias: "great",
+    answers: [{
+            answer_title: "Лендинг-пейдж",
+            type: "radio"
+        },
+        {
+            answer_title: "Корпоративный сайт",
+            type: "radio"
+        },
+        {
+            answer_title: "Интернет-магазин",
+            type: "radio"
+        }
+    ]
+},
+{
+    number: 3,
+    title: "Как дела?",
+    answer_alias: "great",
+    answers: [{
+            answer_title: "Норм",
+            type: "radio"
+        },
+        {
+            answer_title: "Так",
+            type: "radio"
+        },
+        {
+            answer_title: "Нет",
+            type: "radio"
+        }
+    ]
+},
+{
+    number: 4,
+    title: "Оставьте свой телефон, мы вам перезвоним",
+    answer_alias: "phone",
+    answers: [{
+        answer_title: "Введите телефон",
+        type: "text"
+    },
+    ]
+}
+];
